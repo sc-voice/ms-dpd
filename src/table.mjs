ff3259a5af8878d5831772f0dce33bb2f70eb1ef
Karl Lew <karl@firepick.org>
2024-07-23 07:33:16 -0700

    v1.23.0 += table

--- src/table.mjs
+++ src/table.mjs
@@ -9,9 +9,12 @@ export default class Table {
       cellOverflow = '\u2026',
       caption,
       columnSeparator = ' ',
+      lineSeparator = '\n',
       headers=[],
       title,
       rows=[],
+      locales,
+      localeOpts,
     } = opts;
 
     if (!(rows instanceof Array)) {
@@ -46,9 +49,12 @@ export default class Table {
       cellOverflow,
       caption,
       columnSeparator,
+      lineSeparator,
       headers,
       title,
-      rows,
+      rows: [...rows],
+      locales,
+      localeOpts,
     });
   }
 
@@ -98,30 +104,30 @@ export default class Table {
     return Table.fromRows(rows, newOpts);
   }
 
-  datumAsString(row, id) {
+  datumAsString(row, id, opts={}) {
     const msg = "Table.datumAsString()";
-    let { headers, emptyCell='', cellOverflow } = this;
+    let { 
+      headers, emptyCell='', cellOverflow,
+    } = this;
+    let {
+      locales = this.locales,
+      localeOpts = this.localeOpts,
+    } = opts;
     let val = row[id];
     if (val == null) {
       return emptyCell;
     }
     let hdr = headers.find(h=>h.id===id);
-    let s = val;
-    if (typeof s === 'number') {
-      s = s+'';
-    }
+    let s = val.toLocaleString(locales, localeOpts);
     if (hdr) {
       if (hdr.maxWidth && hdr.width < s.length) {
         s = s.slice(0, hdr.maxWidth-1) + cellOverflow;
       }
     }
-    if (s == null) {
-      console.log(msg, {s, row, id});
-    }
     return s;
   }
 
-  #updateHeaders() {
+  #updateHeaders(opts) {
     const msg = "Table.#updateHeaders()";
 
     let { 
@@ -133,7 +139,7 @@ export default class Table {
     rows.forEach(row=>{
       for (let i=0; i<headers.length; i++) {
         let h = headers[i];
-        let datum = this.datumAsString(row, h.id);
+        let datum = this.datumAsString(row, h.id, opts);
         h.width = Math.max(h.width, datum.length);
       }
     });
@@ -146,6 +152,8 @@ export default class Table {
       title = this.title,
       caption = this.caption,
       columnSeparator = this.columnSeparator,
+      locales=this.locales,
+      localeOpts=this.localeOpts,
     } = opts;
     let { 
       headers, 
@@ -168,7 +176,7 @@ export default class Table {
       let data = [];
       headers.forEach(h=>{
         let rawDatum = row[h.id];
-        let datum = this.datumAsString(row, h.id);
+        let datum = this.datumAsString(row, h.id, {locales, localeOpts});
         if (typeof rawDatum === 'number') {
           data.push(datum.padStart(h.width));
         } else {
@@ -185,282 +193,24 @@ export default class Table {
     return lines;
   }
 
-  /*
-  static compare(a,b) {
-    if (a === b) {
-      return 0;
-    } else if (a==null && b) {
-      return -1;
-    } else if (a && b==null) {
-      return 1;
-    }
-    let cmp;
-
-    let aGender = Inflection.attribute(a.gender);
-    let bGender = Inflection.attribute(b.gender);
-    cmp = aGender.order - bGender.order;
-    if (cmp) { return cmp; }
-
-    let aCase = Inflection.attribute(a.case);
-    let bCase = Inflection.attribute(b.case);
-    cmp = aCase.order - bCase.order;
-    if (cmp) { return cmp; }
-
-    let aNumber = Inflection.attribute(a.nbr);
-    let bNumber = Inflection.attribute(b.nbr);
-    cmp = aNumber.order - bNumber.order;
-
-    return cmp;
+  filter(f=(row=>true)) {
+    let { rows } = this;
+    let opts = Object.assign({}, this);
+    opts.rows = rows.filter(f);
+    return new Table(opts);
   }
 
-  static #parseDpdInfDatum(row, opts={}) {
-    const msg = `#parseDpdInfDatum()`;
-    let {
-      pattern,
-      inflections,
-      comps,
-    } = opts;
-    const dbg = 0;
-    const dbgv = 0;
-    let dbgmsg;
-    let dbgvmsg;
-    let rowType = row[0][0];
-    let group;
-    switch (rowType) {
-      case '': 
-        rowType = 'title'; 
-        dbgv && (dbgvmsg = `${pattern} ["${row[0]}", "${row[1]}",...]`);
-        break;
-      case 'nom':
-      case 'acc':
-      case 'instr':
-      case 'dat':
-      case 'abl':
-      case 'gen':
-      case 'loc':
-      case 'voc': {
-        rowType = 'declension';
-        for (let i=1; i<row.length; i+=2) {
-          let data = row[i];
-          let key = row[i+1];
-          dbg && console.log(msg, {comps, key, data});
-        }
-        dbg && (dbgmsg = `${pattern}, ${rowType}`);
-      } break;
-      case 'in comps':
-        group = row[1];
-        break;
-      default:
-        dbgv && (
-          dbgvmsg = `...${pattern} [${row[0]}, ${row[1]}, ...] IGNORED`
-        );
-        break;
-    }
-    dbgmsg && console.log(msg, dbgmsg);
-    dbgvmsg && console.log(msg, dbgvmsg);
-    return {
-      rowType,
-      group,
-      inflections,
-    };
+  sort(compare) {
+    const msg = "Table.sort()";
+    let out = this.rows.sort(compare);
+    return this;
   }
 
-  static parseDpdInflectionTemplate(dpdInf, opts={}) {
-    const msg = "Inflection.parseDpdInflectionTemplate()";
-    let dbg = 0;
-    let {
-      filterCase,
-      verbose,
-      textOut,
-    } = opts;
-    let [ pattern, like, data ] = dpdInf.split('|');
-    
-    let rows = JSON.parse(data);
-    let rowType;
-    let group;
-    let inflections = [];
-    let lastRow = rows[rows.length-1];
-    let in_comps = lastRow[0][0];
-    let comps = lastRow[1];
-    dbg && console.log(msg, {pattern, like, comps});
-    let showSrc = !!textOut;
-    showSrc && rows.forEach((row,iRow)=>{
-      if (filterCase) {
-        if (typeof filterCase === 'string') {
-          filterCase = filterCase.split(new RegExp(', *'));
-        }
-        showSrc = (0 <= filterCase.findIndex(ic=>row[0][0]===ic));
-      }
-      if (showSrc) {
-        const EMPTY = '\u233f';
-        let srcRow = row.map((cell,i)=>{
-          if (i === 0) {
-            let text = verbose ? '' : cell;
-            let rowNum = (
-              iRow==0 ? '#' : iRow.toString()
-            ).padStart(2);
-            return `${rowNum} ${text}`.padEnd(verbose ? 2 : 11 );
-          } else if (i % 2) {
-            return (cell.join(',')||EMPTY).padEnd(23);
-          } else {
-            let text = cell.join(',') || EMPTY;
-            return verbose 
-              ? text.padEnd(13) 
-              : (text&&'\u2026' || text);
-          }
-        });
-        let outRow = srcRow.join(' ');
-        textOut.push(outRow);
-      }
-      let info = Inflection.#parseDpdInfDatum(row, 
-        {pattern, comps, inflections});
-      rowType = rowType || info.rowType;
-      group = group || info.group;
-    });
-    return { pattern, like, comps, textOut, }
+  toLocaleString(locales, localeOpts) {
+    let opts = {locales, localeOpts};
+    let lines = this.asColumns(opts);
+    let { lineSeparator } = this;
+    return lines.join(lineSeparator);
   }
 
-  static attribute(idOrName) {
-    let attrs = Inflection.#ATTRIBUTES;
-    for (let i=0; i<attrs.length; i++) {
-      let attr = attrs[i];
-      if (attr.id===idOrName || attr.name===idOrName) {
-        return attr;
-      }
-    }
-    return found || {
-      type:'unknown', id:null, order:-1, name:'unknown', use:'unknown'
-    }
-  }
-
-  static find(filter=(inf=>true)) {
-    let inflections = Inflection.#ALL.reduce((a,inf)=>{
-      inf = new Inflection(inf);
-      if (filter(inf)) {
-        a.push(new Inflection(inf));
-      }
-      return a;
-    }, []);
-    return inflections;
-  }
-
-  static union(...args) {
-    const msg = "Inflection.union()";
-    let agg = args.flat().reduce((a,inf,i)=>{
-      if (!(inf instanceof Inflection)) {
-        throw new Error(`${msg} Inflection[${i}]? ${inf}`);
-      }
-      let agg = Inflection.#KEYS.forEach(k=>{
-        let akv = a[k];
-        let ikv = inf[k];
-        if (akv instanceof Array) {
-          if (ikv instanceof Array) {
-            let map = {};
-            akv = [...akv, ...ikv].sort().filter(v=>{
-              if (map[v]) {
-                return false;
-              }
-              map[v] = true;
-              return true;
-            });
-            a[k] = akv.length > 1 ? akv : (akv[0] || null);
-          } else if (akv.indexOf(ikv) < 0) {
-            if (ikv) {
-              akv.push(ikv);
-              a[k] = akv.sort();
-            }
-          } else {
-            // ignore existing value
-          }
-        } else if (ikv != null) {
-          if (akv == null) {
-            a[k] = ikv;
-          } else if (akv !== ikv) {
-            a[k] = akv < ikv
-              ? [akv, ikv].flat()
-              : [ikv, akv].flat();
-          }
-        }
-      });
-      return a;
-    }, {})
-    return new Inflection(agg);
-  }
-
-  get isEmpty() {
-    return Inflection.#KEYS.reduce((a,k)=>(a && this[k]==null), true);
-  }
-
-  union(...args) {
-    return Inflection.union(this, ...args);
-  }
-
-  matchesWord(word, opts={}) {
-    const msg = 'Inflection.matchesWord()';
-    const dbg = DBG.MATCHES_WORD;
-    let { stem, singular, plural, gender } = opts;
-    if (singular == null && plural==null) {
-      singular = true;
-      plural = true;
-    }
-    let endings = [];
-    singular && endings.push(this.singular);
-    plural && endings.push(this.plural);
-    endings = endings.flat().filter(end=>!!end);;
-    if (stem) {
-      let endLen = word.length - stem.length;
-      dbg && console.log(msg, {stem, endings});
-      endings = endings.filter(end=>end && (end.length === endLen));
-    }
-
-    return endings
-      .reduce((a,end)=>(a || word.endsWith(end)), false);
-  }
-
-//////////////// PRIVATE ////////////////////
-  static #KEYS = [
-    "id",
-    "type",
-    "group",
-    "gender",
-    "case",
-    "singular",
-    "plural",
-  ];
-
-  static #ALL = Pali.INFLECTIONS.map(inf=>new Inflection(inf));
-  
-  static #ATTRIBUTE_UNKNOWN = 
-    {type:'attribute', id:null, order:0, name:'unknown', 
-      use:'unknown inflection attribute'};
-  static #ATTRIBUTES = [
-    Inflection.#ATTRIBUTE_UNKNOWN,
-    {type:'attribute', id:'attr', order:1, name:'attribute', 
-      use:'inflection attribute'},
-    {type:'attribute', id:'gdr', order:2, name:'gender', 
-      use:'masc/fem/nt'},
-    {type:'attribute', id:'nbr', order:3, name:'number', use:'sg/pl'},
-    {type:'attribute', id:'case', order:4, name:'inflection_case', 
-      use:'nom/acc/instr/dat/abl/gen/loc/voc'},
-
-    {type:'number', id:'sg', order:1, name:'singular', 
-      use:"I, you, he/it/she"},
-    {type:'number', id:'pl', order:2, name:'plural', use:"we/they"},
-
-    {type:'gender', id: 'nt', name:'neuter', order:0, use:'it'},
-    {type:'gender', id: 'masc', name:'masculine', order:1, use:'he'},
-    {type:'gender', id: 'fem', name:'feminine', order:2, use:'she'},
-    {type:'gender', id:'x', name:'(no gender)', order:3, use:'I, you'},
-
-    {type:'case', id:'nom', order:1, name:"nominative", use:"subject"},
-    {type:'case', id:'acc', order:2, name:"accusative", use:"object"},
-    {type:'case', id:'instr', order:3, name:"instrumental", 
-      use:"by, with"},
-    {type:'case', id:'dat', order:4, name:"dative", use:"to, for"},
-    {type:'case', id:'abl', order:5, name:"ablative", use:"from"},
-    {type:'case', id:'gen', order:6, name:"genitive", use:"of"},
-    {type:'case', id:'loc', order:7, name:"locative", use:"in, at, on"},
-    {type:'case', id:'voc', order:8, name:"vocative", use:"(the)"},
-  ];
-*/
 }
--- test/table.mjs
+++ test/table.mjs
@@ -1,8 +1,15 @@
 import should from "should";
 import Table from '../src/table.mjs';
 
+const TEST_DATA = [
+  ['color', 'size', 'date'],
+  ['purple', 10, new Date(2000, 1, 1)],
+  ['red', 5, new Date(2000, 2, 1)],
+  ['blue',, new Date(2000, 3, 1)],
+];
+
 typeof describe === "function" && 
-  describe("TESTTESTtable", function () 
+  describe("table", function () 
 {
   it("default ctor", () => {
     let tbl = new Table();
@@ -27,7 +34,8 @@ typeof describe === "function" &&
     should.deepEqual(tbl.headers.map(h=>h.id), ['color', 'size']);
     should.deepEqual(tbl.headers.map(h=>h.width), [5,4]);
     should(tbl.rows.length).equal(2);
-    should(tbl.rows).equal(rows);
+    should.deepEqual(tbl.rows, rows);
+    should(tbl.rows).not.equal(rows);
 
     let tbl2 = Table.fromRows(rows,
       {title, caption, headers:tbl.headers});
@@ -73,12 +81,7 @@ typeof describe === "function" &&
     should(tbl.rows.length).equal(3);
   });
   it("asColumns()", ()=>{
-    let data = [
-      ['color', 'size'],
-      ['purple', 10],
-      ['red', 5],
-      ['blue',],
-    ];
+    let data = TEST_DATA;
     let title = 'test-title';
     let caption = 'test-caption';
     let opts = {title, caption};
@@ -92,5 +95,44 @@ typeof describe === "function" &&
     should(lines[3]).match(/red *5/);
     should(lines[4]).match(/blue *⌿/);
     should(lines.at(-1)).equal(caption);
+  });
+  it("filter()", ()=>{
+    let title = 'test-title';
+    let caption = 'test-caption';
+    let opts = {title, caption};
+    let tbl = Table.fromArray2(TEST_DATA, opts);
+    let rowFilter = (row=>row.size);
+
+    let tbl2 = tbl.filter(rowFilter);
+
+    should(tbl2.title).equal(tbl.title);
+    should(tbl2.caption).equal(tbl.caption);
+    should.deepEqual(tbl2.rows, tbl.rows.filter(rowFilter));
+  });
+  it("sort()", ()=>{
+    let tbl = Table.fromArray2(TEST_DATA);
+    let compare = ((a,b) => {
+      let cmp = a.color.localeCompare(b.color);
+      return cmp;
+    });
+    let tbl2 = new Table(tbl);
+
+    should(tbl2.sort(compare)).equal(tbl2);
+    should.deepEqual(tbl2.rows[0], tbl.rows[2]);
+  });
+  it("toLocaleString()", ()=>{
+    let tbl = Table.fromArray2(TEST_DATA);
+    let compare = ((a,b) => {
+      let cmp = a.color.localeCompare(b.color);
+      return cmp;
+    });
+    let localeOpts = { dateStyle: "short", }
+    let tblEN = tbl.toLocaleString('en', localeOpts);
+    should(tblEN.split('\n')[0]).match(/Color  Size Date/);
+    should(tblEN.split('\n')[1]).match(/purple   10 2.1.00/);
+
+    let tblFR = tbl.toLocaleString('fr', localeOpts);
+    should(tblFR.split('\n')[0]).match(/Color  Size Date/);
+    should(tblFR.split('\n')[1]).match(/purple   10 01.02.2000/);
   });
 });
