8dc77a988da68cb6efe32149d441fd8ef0a6e26c
Karl Lew <karl@firepick.org>
2024-12-24 09:50:15 -0800

    v2.43.0 += merge

--- src/merge.mjs
+++ src/merge.mjs
@@ -8,6 +8,19 @@ export class Patch {
     this.delete = opts.delete || {};
   }
 
+  static from(arg) {
+    const msg = 'Patch.from:';
+    if (typeof arg === 'string') {
+      return Patch.fromString(arg);
+    }
+
+    if (arg instanceof Patch) {
+      return arg;
+    }
+
+    throw new Error(`${msg} string or Patch? (${arg})`);
+  }
+
   static fromString(sPatch) {
     const msg = "Patch.fromString:";
     const dbg = DBG.PARSE_PATCH || DBG.MERGE>1;
@@ -43,23 +56,25 @@ export class Merge {
     }
     this.baseKeys = Object.keys(srcBase);
     this.srcBase = srcBase;
-    this.srcPatch = srcPatch;
+    this.srcPatch = Patch.from(srcPatch);
   }
 
-  // restore missing base items
-  patchBase(dst) {
-    const msg = "Merge.patchBase:";
+  // restore missing head items
+  patchHead(dst, srcPatch) {
+    const msg = "Merge.patchHead:";
     const dbg = DBG.MERGE;
     let { srcBase, baseKeys } = this;
+    let added = [];
     baseKeys.forEach(key=>{
-      if (dst[key] == null) {
+      if (dst[key]==null && srcPatch.delete[key]==null) {
         let value = srcBase[key];
         dst[key] = value;
-        console.log(msg, `[1]+${key}`, value);
+        dbg && console.log(msg, `[1]+${key}`, value);
+        added.push[key];
       }
     }, {});
 
-    return dst;
+    return { dst, added };
   }
 
   patch(dstBase, srcPatch=this.srcPatch) {
@@ -68,13 +83,19 @@ export class Merge {
     const { srcBase, baseKeys } = this;
 
     let dst = Object.assign({}, dstBase);
-    dst = this.patchBase(dst);
     let delKeys =  Object.keys(srcPatch.delete);
     let addKeys =  Object.keys(srcPatch.add);
-    console.log({delKeys, addKeys});
+    dbg && console.log({delKeys, addKeys});
 
     let conflicts = {};
-    delKeys.forEach(key=>{
+    let result = {
+      conflicts,
+      deleted:[], 
+      updated:[], 
+      added:[],
+      dst,
+    };
+    result = delKeys.reduce((a,key)=>{
       let valDstHead = dst[key];
       let valSrcBase = srcBase[key];
       let valSrcNew = srcPatch.add[key];
@@ -86,22 +107,38 @@ export class Merge {
         }
       } else if (valSrcNew) { 
         dst[key] = valSrcNew;
+        a.updated.push(key);
+        dbg && console.log(msg, '[1]update', key);
       } else {
         delete dst[key];
+        a.deleted.push(key);
+        dbg && console.log(msg, '[2]delete', key);
       }
-    });
+      return a;
+    }, result);
     addKeys.forEach(key=>{
       let valDstHead = dst[key];
       let valSrcNew = srcPatch.add[key];
       if (valDstHead == null) {
         dst[key] = valSrcNew;
+        result.added.push(key);
+        dbg && console.log(msg, '[3]add', key);
+      } else if (valDstHead === valSrcNew ) {
+        dbg>1 && console.log(msg, '[4]ignore', key, valDstHead);
+      } else if (conflicts[key]==null) {
+        conflicts[key] = {
+          dst: valDstHead,
+          src: valSrcNew,
+        }
+        // unexpected conflict should never happen
+        console.log(msg, '[5]CONFLICT?', key, {
+          valDstHead, valSrcNew});
       }
     });
+    let { added } = this.patchHead(dst, srcPatch);
 
+    result.added = [...result.added, ...added ];
 
-    return {
-      conflicts,
-      dst
-    };
+    return result;
   }
 }
--- test/merge.mjs
+++ test/merge.mjs
@@ -13,16 +13,16 @@ const dbg = 0;
 const EN_BASE = {
   1: "one EN",
   2: "two EN",
-  3: "three EN",
-  4: "four EN",
+  3: "three EN (oops)",
+  4: "four EN (del)",
   5: "five EN",
 }
 
 const EN_HEAD = {
   1: "one EN changed",
   2: "two EN changed",
-  3: "three EN",
-  //4: "four EN",
+  3: "three EN (oops)",
+  //4: "four EN (del)", 
   5: "five EN",
   6: "six EN new",
 }
@@ -30,7 +30,8 @@ const EN_HEAD = {
 const PT_HEAD = {
   1: "one PT",
   2: "two EN",
-  4: "four EN",
+  //3: "three EN (oops)",  // mistaken delete
+  4: "four EN (del)",
   5: "five EN",
 }
 
@@ -48,8 +49,8 @@ index c00c188..480cfe9 100644
 +  "1": "one EN changed",
 -  "2": "two EN",
 +  "2": "two EN changed",
-   "3": "three EN",
--  "4": "four EN",
+   "3": "three EN (oops)",
+-  "4": "four EN (del)",
    "5": "five EN"
 +  "6": "six EN new"
  }
@@ -75,25 +76,40 @@ typeof describe === "function" &&
       delete: {
         '1': 'one EN',
         '2': 'two EN',
-        '4': 'four EN',
+        '4': 'four EN (del)',
       },
     }));
   });
+  it("TESTTESTPatch.from()", ()=>{
+    let patch1 = Patch.fromString(DIFF_EN);
+    should.deepEqual(Patch.from(DIFF_EN), patch1);
+    should(Patch.from(patch1)).equal(patch1);
+    let eCaught;
+    try {
+      Patch.from({bad:'input'});
+    } catch(e) { eCaught = e; }
+    should(eCaught instanceof Error);
+    should(eCaught.message).match(/string or Patch/);
+  });
   it("TESTTESTpatch() pt", ()=>{
     let srcBase = EN_BASE;
-    let srcPatch = Patch.fromString(DIFF_EN);
+    let srcPatch = DIFF_EN;
     let mrg = new Merge({ srcBase, srcPatch });
 
     let res = mrg.patch(PT_HEAD);
     should.deepEqual(res.dst, {
-      1: "one PT",
-      2: "two EN changed",
-      3: "three EN",
-      5: "five EN",
-      6: "six EN new",
+      1: "one PT",          // src:---,    dst:update => update
+      2: "two EN changed",  // src:update, dst:---    => update
+      3: "three EN (oops)", // src:---, d  dst:delete => restore
+      //4: "four EN (del)", // src:delete, dst:---    => delete
+      5: "five EN",         // src:---,    dst:---    => ---
+      6: "six EN new",      // src:new,    dst:---    => add
     });
     should.deepEqual(res.conflicts, {
       1: { dst: 'one PT', src: 'one EN changed' },
     });
+    should.deepEqual(res.added, [ '6', ]);
+    should.deepEqual(res.deleted, [ '4', ]);
+    should.deepEqual(res.updated, [ '2', ]);
   });
 });
