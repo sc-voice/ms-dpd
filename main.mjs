5fe102db59e3b02a5803b5abfb7cad0dc5b2a65c
Karl Lew <karl@firepick.org>
2025-01-19 13:53:35 -0800

    v2.49.0 += refactor

--- main.mjs
+++ main.mjs
@@ -1,13 +1,7 @@
 // ESM syntax is supported.
 export { default as Pali } from "./src/pali.mjs";
-export { default as Sankey } from "./src/sankey.mjs";
 export { default as Dictionary } from "./src/dictionary.mjs";
 export { default as HeadwordKey } from "./src/headword-key.mjs";
 export { default as Inflection } from "./src/inflection.mjs";
 export { default as Table } from "./src/table.mjs";
-
-
-// DeepL adapter
-export { default as DeepLAdapter } from "./src/deepl-adapter.mjs";
-export { default as QuoteParser } from "./src/quote-parser.mjs";
 
