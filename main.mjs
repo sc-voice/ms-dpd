635b209a3168f72af14cf7936b092b900657ae4b
Karl Lew <karl@firepick.org>
2025-01-19 13:45:58 -0800

    v2.49.0 += deepl-adapter

--- main.mjs
+++ main.mjs
@@ -6,3 +6,8 @@ export { default as HeadwordKey } from "./src/headword-key.mjs";
 export { default as Inflection } from "./src/inflection.mjs";
 export { default as Table } from "./src/table.mjs";
 
+
+// DeepL adapter
+export { default as DeepLAdapter } from "./src/deepl-adapter.mjs";
+export { default as QuoteParser } from "./src/quote-parser.mjs";
+
